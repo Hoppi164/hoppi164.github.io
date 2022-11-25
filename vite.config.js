1b76d8094ce5dc9704045ee9872160a01b16b73b
Hoppi164 <Hoppi164@gmail.com>
2022-11-25 22:47:07 +1000

    Added ability to analyze bundle size

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { visualizer } from "rollup-plugin-visualizer";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		visualizer({
+			emitFile: true,
+			file: "stats.html",
+			template: 'sunburst',
+		}),
+	]
 };
 
 export default config;
