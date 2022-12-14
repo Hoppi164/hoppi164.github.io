f50c044659cd53f2316c11d145133b6f1c0270d6
Hoppi164 <Hoppi164@gmail.com>
2022-12-14 23:33:45 +1000

    Add onscroll transition to progress bars

--- src/lib/components/ProgressBar.svelte
+++ src/lib/components/ProgressBar.svelte
@@ -1,13 +1,29 @@
 <script>
+	import { onMount } from 'svelte';
 	/** @type {string} */
 	export let label;
 
 	/** @type {string} */
 	export let value;
+
+	let el;
+	let currentValue = '0';
+
+	// create an intersection observer that updates the "currentValue" when this element is visible in the viewport
+	onMount(async () => {
+		const options = {
+			rootMargin: '-10%'
+		};
+		const observer = new IntersectionObserver((entries) => {
+			const isIntersecting = entries[0].isIntersecting;
+			currentValue = isIntersecting ? value : '0';
+		}, options);
+		observer.observe(el);
+	});
 </script>
 
-<div class="flex-center">
-	<div class="background" style="--value: {value}%">
+<div class="flex-center" bind:this={el}>
+	<div class="background" style="--value: {currentValue}%">
 		<div class="foreground">
 			<label for="meter{label}">{label}</label>
 		</div>
@@ -31,6 +47,7 @@
 		border-top-left-radius: 5px;
 		border-bottom-left-radius: 5px;
 		height: 100%;
+		transition: width 1s ease-in-out;
 	}
 	.foreground > label {
 		padding-left: 20px;
