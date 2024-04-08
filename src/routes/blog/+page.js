8d3b2cde0a061bbdb005c4e763bd65725732649e
Drew Hopkins <andrew.hopkins@akcelo.com>
2024-04-08 15:05:50 +1000

    autoformat

--- src/routes/blog/+page.js
+++ src/routes/blog/+page.js
@@ -7,7 +7,7 @@ export async function load() {
 		if (!allPosts || allPosts.length === 0) {
 			throw new Error('No Posts Found');
 		}
-		return {allPosts};
+		return { allPosts };
 	} catch (err) {
 		throw error(500, `Cannot Load Post Index \n\nMessage:\n${err.message}\n\nStack:\n${err.stack}`);
 	}
